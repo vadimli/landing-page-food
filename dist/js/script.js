d2b8bc43b2e93b5424ec631cbad9852702af335c
Vadim Likhachev <vadlen2149@gmail.comm>
2021-03-09 17:02:27 +0300

    Make tabs

---
+++ dist/js/script.js
@@ -0,0 +1,43 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabsContent() {
+        tabContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show');
+        });
+        
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+    
+    function showTabsContent(i = 0) {
+        tabContent[i].classList.add('show', 'fade');
+        tabContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabsContent();
+    showTabsContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+        
+        if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabsContent();
+                    showTabsContent(i);
+                }
+            });
+
+        }
+    
+    });
+
+    
+});
