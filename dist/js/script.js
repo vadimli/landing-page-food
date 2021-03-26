2016ec7aad19851ccb370c8bcfe6c41f0f0b954e
Vadim Likhachev <vadlen2149@gmail.comm>
2021-03-26 15:16:41 +0300

    Use rest

--- dist/js/script.js
+++ dist/js/script.js
@@ -147,12 +147,13 @@ window.addEventListener('scroll', openModalByScroll);
 //Классы для картинок
 
 class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
         this.src = src;
         this.alt = alt;
         this.title = title;
         this.descr = descr;
         this.price = price;
+        this.classes = classes;
         this.parent = document.querySelector(parentSelector);
         this.transfer = 72;
         this.changeToRub(); 
@@ -164,8 +165,13 @@ class MenuCard {
 
     render() {
         const element = document.createElement('div');
+        if (this.classes.length === 0) {
+            this.element = "menu__item";
+            element.classList.add(this.element);
+        } else {
+            this.element.forEach(className => element.classList.add(className));
+        }
         element.innerHTML = `
-            <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -174,7 +180,6 @@ class MenuCard {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
                 </div>
-            </div>
         `;
         this.parent.append(element);
     }
